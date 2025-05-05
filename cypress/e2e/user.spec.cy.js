8983a672dd71a21325b233ee0062781b39754cf5
Adla <adla.martinsalvarenga@gmail.com>
2025-05-05 11:20:20 -0300

    Add random values on user spec

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,37 +4,31 @@ import DashboardPage from '../pages/dashboardPage'
 import MyInfoPage from '../pages/myInfoPage'
 import MenuPage from '../pages/menuPage'
 
+const Chance = require('chance')
+
 const loginPage = new LoginPage ()
 const dashboardPage = new DashboardPage ()
 const myInfoPage = new MyInfoPage ()
 const menuPage = new MenuPage ()
+const chance = new Chance ()
 
-describe('Orange HRM Tests', () => {
+describe('User Info Update - Orange HRM Tests', () => {
 
-  
-  
-  it.only('User Info Update - Sucess', () => {
+   
+  it('User Info Update - Sucess', () => {
 
     loginPage.accessLoginPage()
-    loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)
+    loginPage.loginWithAnyUser(userData.userSucess.username, userData.userSucess.password)
     
     dashboardPage.checkDashboardPage()
 
     menuPage.accessMyInfo()
     
-    myInfoPage.fillPersonalDetails('First Name', 'Middle Name', 'Last Name', 'Nickname')
-    myInfoPage.fillEmployeeDetails('126652', '4164', '56465465', '2026-04-21', '55465', '22454')
+    myInfoPage.fillPersonalDetails(chance.first(), chance.last(), chance.string())
+    myInfoPage.fillEmployeeDetails(chance.integer(), chance.integer(), chance.integer(), chance.date({string: true}), chance.ssn(), chance.integer())
     myInfoPage.fillStatus()
     myInfoPage.saveForm()
          
   })
-  it('Login - Fail', () => {
-
-    cy.visit('auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
-
-  })
+ 
 })
--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -17,10 +17,14 @@ class LoginPage {
         cy.visit('/auth/login')
     }
 
-    loginWithUser(username, password) {
+    loginWithAnyUser(username, password) {
         cy.get(this.selectorsList().usernameField).type(username)
         cy.get(this.selectorsList().passwordField).type(password)
         cy.get(this.selectorsList().loginButton).click()
+    }
+
+    checkAccessInvalid(){
+        cy.get(this.selectorsList().wrongCredentialAlert)
     }
 }
 
